16c7c09c4a3b14c6bf3c6fe3d3c28199d6b4eef8
Dhruv Govila <dgovila@deloitte.com>
2019-07-12 16:22:08 +0530

    - Project Setup

---
+++ NoticeBoard/NoticeBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoticeBoard
+//
+//  Created by Dhruv Govila on 12/07/19.
+//  Copyright © 2019 Deloitte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
